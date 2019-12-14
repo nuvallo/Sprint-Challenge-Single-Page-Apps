4f2a4d3f4adefa4168da396bd3438dafb6485329
Tyler <nuvallo.inc@gmail.com>
2019-12-13 21:04:55 -0500

    axios fetching Data from API

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,11 +1,20 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-export default function CharacterList() {
+export default function CharacterList(props) {
   // TODO: Add useState to track data from useEffect
+  const [characters, setCharacters] = useState([]);
 
   useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios
+      .get("https://rickandmortyapi.com/api/character/")
+      .then(response => {
+        // console.log(response.data.results);
+        setCharacters(response.data.results);
+      })
+      .catch(error => {
+        console.error("Server Error", error);
+      });
   }, []);
 
   return (
