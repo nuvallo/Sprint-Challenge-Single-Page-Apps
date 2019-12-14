1b5bb32b60ecc3b6f60957b010c272cd8cae3f08
Tyler <nuvallo.inc@gmail.com>
2019-12-13 22:21:25 -0500

    searchForm created

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,52 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import CharacterCard from "./CharacterCard";
+import { Link } from "react-router-dom";
+import { Button } from "reactstrap";
 
 export default function SearchForm() {
- 
+  const [data, setData] = useState([]);
+  const [query, setQuery] = useState("");
+
+  useEffect(() => {
+    axios.get("https://rickandmortyapi.com/api/character/").then(response => {
+      const characters = response.data.results.filter(char =>
+        char.name.toLowerCase().includes(query.toLowerCase())
+      );
+      setData(characters);
+    });
+  }, [query]);
+
+  const handleInputChange = event => {
+    setQuery(event.target.value);
+  };
   return (
-    <section className="search-form">
-     // Add a search form here
-    </section>
+    <div>
+      <form>
+        <input
+          id="name"
+          type="text"
+          name="textfield"
+          placeholder="Search"
+          value={query}
+          onChange={handleInputChange}
+        />
+
+        <Link to="/">
+          <Button>Home</Button>
+        </Link>
+      </form>
+
+      {data.map(character => {
+        return (
+          <CharacterCard
+            key={character.id}
+            name={character.name}
+            species={character.species}
+            status={character.status}
+          />
+        );
+      })}
+    </div>
   );
 }
